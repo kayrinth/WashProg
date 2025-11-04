e3d1799f77a490dc36f7e057d9c64003b7f43bef
Damar Adi N <damaradii.nugroho@gmail.com>
2025-11-04 12:48:01 +0700

    [UPDATE] fix name

--- ADMIN/src/components/organism/index.jsx
+++ ADMIN/src/components/organism/index.jsx
@@ -1,6 +1,6 @@
 import Orders from "./orders.jsx";
 import Users from "./users.jsx";
 import OrdersAdmin from "./ordersAdmin.jsx";
-import OrderNew from "./orderNew.jsx";
+import OrderNew from "./OrderNew.jsx";
 import Dashboard from "./dashboard.jsx";
 export { Orders, Users, OrdersAdmin, OrderNew, Dashboard };
